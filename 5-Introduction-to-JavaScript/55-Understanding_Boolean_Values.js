352d491ce57b131390ace621459185096755c3a8
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 14:38:21 -0600

    Boolean

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+// data type boolean. two values true or false
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
