2114111056faf94fcf1ab295798fcc95c9d80787
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 14:29:55 -0600

    Add nested array into brackets

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+// add the nested array into the brackets
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [ourArray];
