8077b7f96c901d528c03bdbe65fb077d1420ef52
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 15:23:02 -0600

    Tabular Data

---
+++ 5-Introduction-to-JavaScript/87-Using_Objects_for_Lookups.js
@@ -0,0 +1,23 @@
+// tabular data you can use an object to lookup values
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+   var lookup = {
+    alpha:   "Adams",
+    bravo:   "Boston",
+    charlie: "Chicago",
+    delta:   "Denver",
+    echo:    "Easy",
+    foxtrot: "Frank"
+  };
+
+  result = lookup[val];
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+phoneticLookup("charlie");
