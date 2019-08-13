3bfb5e84b3a0e6134de82cbaf15856c983b4724e
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-12 21:47:48 -0600

    camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+// this is properCamelCase for variables
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
