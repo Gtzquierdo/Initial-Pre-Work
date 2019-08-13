b13a006c8ef73c0fd85827d9af98fa2f42a1f614
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 14:23:32 -0600

    Concatenation

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,7 @@
+// in order to concatenate use + between strings that are in quotations
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
