b981bde57eaf590cd7d162ed2b71255cb87c92ec
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 14:25:43 -0600

    += broke long string over lines

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+// to break a long string over several lines use +=
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
