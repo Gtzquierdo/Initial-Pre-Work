55dcc12fdf8149260c04a00fbe4e0b1b6a4c9e15
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 14:26:07 -0600

    Operator added one ore more vars into a string

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+// + operator can insert one or more variables into a strings
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Peter";
+var myStr = "My name is " + myName + " and I am swell!";
