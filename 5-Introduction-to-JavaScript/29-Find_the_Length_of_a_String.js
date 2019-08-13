b6d2707ee8d7ca1d466bce262bbb73f34a5a13ae
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 14:26:52 -0600

    .length string variables

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,14 @@
+// finding length of string .length of a string variables
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
