f62bd47d5f25c5d142d46684603b41dced6d2359
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 15:20:45 -0600

    Dot Notation

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,12 @@
+// .notation and bracket notation. access the properties of an objects
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
