961883f7d574f494f6507eadd435aba518145de4
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 14:28:59 -0600

    Mad Libs

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,11 @@
+// Mad Libs
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "";
+    result = " " + myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb;
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
