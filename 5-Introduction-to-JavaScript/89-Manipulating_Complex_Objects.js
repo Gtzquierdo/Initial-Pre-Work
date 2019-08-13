ad3989ea3878f8b4090661e82d7e17afd6439f26
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 15:23:59 -0600

    Complex

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,25 @@
+// Manipulation for these complex objects
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Artist",
+    "title": "Song Titles",
+    "release_year": 2019,
+    "formats": [
+      "CD",
+      "Atrack",
+      "Cassette"
+    ],
+  }
+];
