9aa4c1a6300e9c854108dee3d087865ea1fddf02
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 14:26:25 -0600

    Appended variables to string

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,11 @@
+// appending variables toa  string using the plus equals
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "awesome";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
