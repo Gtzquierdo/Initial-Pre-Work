a6f05184c4a9a96068a238103c1c498585fcbd3a
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 14:36:10 -0600

    Local variable took precendece over global when same name

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,14 @@
+// local takes precedence over global var when same name
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
