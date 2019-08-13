2f0a72c797e1a85a1839d1dab995fc090470d444
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-12 21:47:26 -0600

    Vaariables no longer undefined

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,11 @@
+// initialize the variables so they will not be undefined. Otherwise its NaN or undefined
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
