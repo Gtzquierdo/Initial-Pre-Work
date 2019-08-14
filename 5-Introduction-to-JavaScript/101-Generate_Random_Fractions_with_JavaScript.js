286a7fe8d026f99724eb02f30ae9716e8303198b
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 21:25:09 -0600

    Inclusive and Exclusive

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+// random decimal number between 0 (inclusive) and not quite up to 1 (exclusive)
+function randomFraction() {
+
+  // Only change code below this line.
+
+return Math.random();
+
+  // Only change code above this line.
+}
