801c9155e0fbc1665d1577b2245dde1a16efe51d
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-14 10:39:47 -0600

    Let have some extra features

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+// Let keyword has some extra features, when declared inside a block, statement, or expression, its scope is limited to that
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+checkScope();
