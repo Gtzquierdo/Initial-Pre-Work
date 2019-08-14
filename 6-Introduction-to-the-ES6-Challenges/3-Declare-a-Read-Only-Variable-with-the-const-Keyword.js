a02782f98c859f2c21067bd82b6b2564af717d05
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-14 10:47:31 -0600

    Const. No need to reassign

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,14 @@
+// Const is read-only and its great to use when you don't want to reassign a variable value. The const variable should be capitalize for good practicefunction printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
