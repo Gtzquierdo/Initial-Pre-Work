3b7b6ea1bcbda88610cd60813054d39d065e61f6
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 20:50:35 -0600

    do/while loop

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,10 @@
+// do...while loop ensures that the code inside the loop will run at least once
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+do {
+  myArray.push(i);
+  i++;
+}while(i < 11);
