042b7177eb5b5e4e408024151dba13da02413700
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 20:49:01 -0600

    For loop

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,16 @@
+// for loop code runs "for" a specific number of times
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
