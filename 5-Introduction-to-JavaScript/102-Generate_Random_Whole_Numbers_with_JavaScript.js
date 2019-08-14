62bfe27df5ae498c07d93821e89b91e5dfd8ef0d
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 21:25:26 -0600

    math.floor

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,9 @@
+// math.floor to round the number down to its nearest whole number
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
