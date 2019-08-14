45fb9bf2cd2e60d9240630d107a1daca634c9618
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-13 21:27:45 -0600

    Redix

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,6 @@
+// radix: specifies the base of the number in the string. can be an integer between 2 and 36
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+
+convertToInteger("10011");
