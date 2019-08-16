71eec6b0685ef2835b3c9a6ce084e6ff947dd710
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-16 11:52:21 -0600

    Code cleaner with Arrow Function

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,5 @@
+// In JS we don't necessary need to name our functions, same with inline functions. ES6 gave us arrow functions so we can have cleaner code
+const magic = () => {
+  "use strict";
+  return new Date();
+};
