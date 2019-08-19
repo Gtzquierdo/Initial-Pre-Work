6dac791efc7ac443ebcdf03c48c24b11770c1951
Gtzquierdo <gtzquierdo@gmail.com>
2019-08-19 13:22:45 -0600

    Spread operator

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,8 @@
+// Spread operator and having the arr1 copied into arr2
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
